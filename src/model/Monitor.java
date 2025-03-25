f2ca9e9a4c6ae83c88664d9d3108296dd237cf12
MalteDs <dmalte2002@hotmail.com>
2025-03-24 23:41:35 -0500

    feat: implement monitor and student classes for synchronization

--- src/model/Monitor.java
+++ src/model/Monitor.java
@@ -1,14 +1,54 @@
+package model;
+
 import java.util.concurrent.Semaphore;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
 
 public class Monitor implements Runnable{
 
     public static Semaphore semaphoreMonitor = new Semaphore(0);
     public static Semaphore semaphoreOffice = new Semaphore(1);
     public static Semaphore semaphoreChair = new Semaphore(0);
+    public static Queue<Integer> queueStudents = new LinkedList<>();
+    public static final Object lock = new Object();
+    public static boolean monitorSleeping = true;
+
     @Override
     public void run() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
+        while(true){
+            try{
+                System.out.println("Monitor is sleeping");
+                semaphoreMonitor.acquire();
+
+                synchronized(lock){
+                    monitorSleeping = false;
+                    System.out.println("Monitor is awake and ready to help students");
+                }
+
+                int studentId = -1;
+                while(true){
+                    synchronized(lock){
+                        if(queueStudents.isEmpty()){
+                            monitorSleeping = true;
+                            break;
+                        }
+                        studentId = queueStudents.poll();
+                        semaphoreChair.release();
+                    }
+                }
+
+                // atender estudiante
+                System.out.println("Monitor is helping student " + studentId);
+                Thread.sleep(new Random().nextInt(5000) + 1000);
+                System.out.println("Monitor finished helping student " + studentId);
+                semaphoreOffice.release();
+
+            
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
     }
     
 }
--- src/model/Student.java
+++ src/model/Student.java
@@ -1,3 +1,62 @@
-public class Student {
-    
+package model;
+import java.util.Random;
+
+public class Student implements Runnable {
+    private final Integer id;
+    private final Random random = new Random();
+
+    public Student(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            try{
+                // tiempo programando en sala
+                System.out.println("Student " + id + " is programming");
+                Thread.sleep(random.nextInt(5000) + 1000);
+                // buscar ayuda
+                System.out.println("Student " + id + " is going to the monitor");
+                
+                synchronized(Monitor.lock){
+                    if(Monitor.monitorSleeping){
+                        // despertar al monitor
+                        System.out.println("Student " + id + " wakes up the monitor");
+                        Monitor.semaphoreMonitor.release();
+                        Monitor.monitorSleeping = false;
+                    }
+                }
+
+                // intentar entrar a la oficina
+                if(Monitor.semaphoreOffice.tryAcquire()){
+                    // entrar a la oficina
+                    System.out.println("Student " + id + " enters the office");
+                    Thread.sleep(random.nextInt(5000) + 1000);
+                    System.out.println("Student " + id + " end help, leaves the office");
+                    Monitor.semaphoreOffice.release();
+                }else{
+                    // intentar sentarse
+                    if(Monitor.semaphoreChair.tryAcquire()){
+                        synchronized(Monitor.lock){
+                            Monitor.queueStudents.add(id);
+                            System.out.println("Student " + id + " is waiting for help. Students in queue: " + Monitor.queueStudents.size());
+                        }
+                        // esperar a ser atendido
+                        Monitor.semaphoreOffice.acquire();
+                        // ya fue atendido, continuar
+                        continue;
+                    } else {
+                        // no hay sillas, salir
+                        System.out.println("Student " + id + ": no chairs available, will try later");
+                    }   
+                }
+            }catch(InterruptedException e){
+                System.out.println("Error in student " + id);
+                Thread.currentThread().interrupt();
+                return;
+            }
+        }
+    }
+
 }
